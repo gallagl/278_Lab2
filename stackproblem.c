55f70cba878cc136ea03d83f24d5393fab5d57a8
gallagl <gallagherliamp@gmail.com>
2022-10-05 23:35:11 -0400

    Finished problem 1

---
+++ stackproblem.c
@@ -0,0 +1,50 @@
+// stackproblem.c – Lab 02 – FirstName, LastName
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int top = -1;
+char MAXSIZE = 80;
+
+void push(char *input,char d){
+    if (top == (MAXSIZE - 1)){
+        printf("Overflow Error");
+    }else{
+        input[++top] = d;
+    }
+}
+
+void pop(char *stack){
+    if (top == -1){
+        printf("Underflow error");
+    }else{
+        printf("%c",stack[top--]);
+    }
+}
+int main(){
+
+    char stack[MAXSIZE];
+
+
+    printf("Please enter each character one at a time. Enter '*' to stop the program. \n");
+
+    char input = '1';
+
+    while (input != '*'){
+        scanf("%c",&input);
+        if (input != '*'){
+            push(&stack,input);
+        }
+    }
+
+    printf("\nReversed Stack:\n");
+
+    while (top != -1){
+        pop(&stack);
+    }
+
+
+
+
+}
+
